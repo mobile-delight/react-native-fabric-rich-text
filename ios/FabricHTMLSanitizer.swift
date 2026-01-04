michael-fay/react-native-html-text

feat: MVP Fabric HTMLText component

ios/FabricHTMLSanitizer.swift
@@ -1,0 +1,104 @@
+import Foundation
+import SwiftSoup
+
+/// HTML Sanitizer using SwiftSoup.
+///
+/// Provides XSS protection by sanitizing HTML content before
+/// it reaches the native iOS HTML parser (NSAttributedString).
+///
+/// Security approach:
+/// - Whitelist-based: Only allows known-safe tags and attributes
+/// - URL protocol validation: Only http, https, mailto, tel allowed
+/// - All event handlers and script content stripped
+@objc public final class FabricHTMLSanitizer: NSObject {
+  /**
+   * Allowed HTML tags - uses FabricGeneratedConstants (from src/core/constants.ts).
+   * Run `yarn codegen:constants` to regenerate.
+   */
+  private static var allowedTags: [String] { FabricGeneratedConstants.allowedTags }
+
+  /**
+   * Allowed URL protocols - uses FabricGeneratedConstants (from src/core/constants.ts).
+   */
+  private static var allowedProtocols: [String] { FabricGeneratedConstants.allowedProtocols }
+
+  /**
+   * Custom whitelist configured with our security policy.
+   */
+  private let whitelist: Whitelist
+
+  /**
+   * Output settings configured to disable pretty-printing.
+   * SwiftSoup's default pretty-print adds newlines/indentation between tags,
+   * which becomes unwanted text content when parsed.
+   */
+  private let outputSettings: OutputSettings
+
+  public override init() {
+    outputSettings = OutputSettings()
+    _ = outputSettings.prettyPrint(pretty: false)
+    // Start with no tags allowed, then add our safe list
+    whitelist = Whitelist.none()
+
+    // Add allowed tags
+    do {
+      for tag in FabricHTMLSanitizer.allowedTags {
+        try whitelist.addTags(tag)
+      }
+
+      // Allow href on anchor tags with protocol restrictions
+      try whitelist.addAttributes("a", "href")
+      for proto in FabricHTMLSanitizer.allowedProtocols {
+        try whitelist.addProtocols("a", "href", proto)
+      }
+
+      // Allow class attribute for styling (id removed per YAGNI - not used in rendering)
+      for tag in FabricHTMLSanitizer.allowedTags {
+        try whitelist.addAttributes(tag, "class")
+      }
+    } catch {
+      // Initialization errors should not occur with valid config
+      // Log error in debug builds
+      #if DEBUG
+        print("FabricHTMLSanitizer: Failed to configure whitelist: \(error)")
+      #endif
+    }
+
+    super.init()
+  }
+
+  /**
+   * Sanitize HTML content to remove XSS vectors.
+   *
+   * - Parameter html: The raw HTML string to sanitize
+   * - Returns: Sanitized HTML string safe for rendering, or empty string on error
+   */
+  @objc public func sanitize(_ html: String) -> String {
+    guard !html.isEmpty else {
+      return ""
+    }
+
+    do {
+      // Normalize inter-tag whitespace from source formatting (JSX indentation, etc.)
+      // BEFORE sanitizing so SwiftSoup processes clean input.
+      // Browsers ignore whitespace between block elements; we do the same.
+      let normalized = html.replacingOccurrences(
+        of: ">\\s+<",
+        with: "><",
+        options: .regularExpression
+      )
+
+      // Sanitize with pretty-printing disabled to prevent SwiftSoup from adding whitespace
+      var clean = try SwiftSoup.clean(normalized, "", whitelist, outputSettings) ?? ""
+
+      // Trim leading/trailing whitespace
+      clean = clean.trimmingCharacters(in: .whitespacesAndNewlines)
+
+      return clean
+    } catch {
+      // Log in all builds - silent failures cause debugging nightmares in production
+      NSLog("FabricHTMLSanitizer: Failed to sanitize HTML: %@", String(describing: error))
+      return ""
+    }
+  }
+}

ios/ci.sh
@@ -1,0 +1,185 @@
+#!/bin/bash
+set -euo pipefail
+
+SIMULATOR_NAME="iPhoneTest"
+SCHEME="NativeTestHarness"
+
+# Navigate to the test harness
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+TEST_HARNESS_DIR="$SCRIPT_DIR/../native-tests/ios"
+
+cd "$TEST_HARNESS_DIR"
+
+# Get the latest available iOS runtime
+get_latest_ios_runtime() {
+    xcrun simctl list runtimes -j | \
+        python3 -c "
+import json, sys
+data = json.load(sys.stdin)
+ios_runtimes = [r for r in data['runtimes'] if r['platform'] == 'iOS' and r['isAvailable']]
+if ios_runtimes:
+    latest = sorted(ios_runtimes, key=lambda x: [int(p) for p in x['version'].split('.')], reverse=True)[0]
+    print(latest['identifier'])
+else:
+    sys.exit(1)
+"
+}
+
+# Get a compatible device type for the given runtime
+get_compatible_device_type() {
+    local runtime="$1"
+    xcrun simctl list devicetypes -j | \
+        python3 -c "
+import json, sys
+data = json.load(sys.stdin)
+runtime = '$runtime'
+
+# Extract major version from runtime (e.g., 'iOS-26-0' -> 26)
+import re
+match = re.search(r'iOS-(\d+)', runtime)
+runtime_major = int(match.group(1)) if match else 18
+
+# Modern device types that work with recent iOS versions
+# Prefer iPhone models from the last few years
+preferred_patterns = ['iPhone-16', 'iPhone-15', 'iPhone-14', 'iPhone-13', 'iPhone-12']
+
+for pattern in preferred_patterns:
+    for d in data['devicetypes']:
+        if pattern in d['identifier']:
+            print(d['identifier'])
+            sys.exit(0)
+
+# Fallback: any iPhone that's not too old
+for d in reversed(data['devicetypes']):
+    if 'iPhone' in d['name'] and 'SE' not in d['name']:
+        # Skip very old models
+        old_models = ['iPhone-4', 'iPhone-5', 'iPhone-6', 'iPhone-7', 'iPhone-8']
+        if not any(old in d['identifier'] for old in old_models):
+            print(d['identifier'])
+            sys.exit(0)
+
+# Last resort: any iPhone
+for d in reversed(data['devicetypes']):
+    if 'iPhone' in d['name']:
+        print(d['identifier'])
+        sys.exit(0)
+
+sys.exit(1)
+"
+}
+
+# Check if simulator exists and is available
+simulator_exists() {
+    xcrun simctl list devices -j | \
+        python3 -c "
+import json, sys
+data = json.load(sys.stdin)
+name = '$SIMULATOR_NAME'
+for runtime, devices in data['devices'].items():
+    for device in devices:
+        if device['name'] == name and device.get('isAvailable', False):
+            print(device['udid'])
+            sys.exit(0)
+sys.exit(1)
+" 2>/dev/null
+}
+
+echo "=== iOS CI Test Runner ==="
+echo "Working directory: $(pwd)"
+
+# Run codegen to generate Fabric component files
+echo ""
+echo "=== Running Codegen ==="
+(cd ../.. && yarn codegen)
+
+# Configure Xcode project with source files and codegen output
+echo ""
+echo "=== Configuring Xcode Project ==="
+BUNDLE_GEMFILE=../Gemfile bundle exec ruby setup_test_harness.rb
+
+# Install CocoaPods dependencies
+echo ""
+echo "=== Installing CocoaPods Dependencies ==="
+BUNDLE_GEMFILE=../Gemfile bundle install --quiet
+BUNDLE_GEMFILE=../Gemfile bundle exec pod install
+
+# Check if simulator already exists
+if UDID=$(simulator_exists); then
+    echo "Using existing simulator: $SIMULATOR_NAME ($UDID)"
+else
+    echo "Creating simulator: $SIMULATOR_NAME"
+
+    RUNTIME=$(get_latest_ios_runtime)
+    echo "  Runtime: $RUNTIME"
+
+    DEVICE_TYPE=$(get_compatible_device_type "$RUNTIME")
+    echo "  Device type: $DEVICE_TYPE"
+
+    UDID=$(xcrun simctl create "$SIMULATOR_NAME" "$DEVICE_TYPE" "$RUNTIME")
+    echo "  Created: $UDID"
+fi
+
+# Custom DerivedData path for predictable caching
+# Configurable via environment variable for CI flexibility
+DERIVED_DATA_PATH="${XCODE_DERIVED_DATA_PATH:-$TEST_HARNESS_DIR/DerivedData}"
+mkdir -p "$DERIVED_DATA_PATH" || {
+    echo "ERROR: Cannot create DerivedData directory at $DERIVED_DATA_PATH"
+    exit 1
+}
+
+echo ""
+echo "=== Building Dependencies (Stage 1) ==="
+# Build pods scheme first - this compiles all React Native dependencies
+# and is cached by irgaly/xcode-cache action in CI
+if command -v xcbeautify &> /dev/null; then
+    xcodebuild build \
+        -workspace NativeTestHarness.xcworkspace \
+        -scheme "Pods-NativeTestHarness" \
+        -destination "platform=iOS Simulator,id=$UDID" \
+        -derivedDataPath "$DERIVED_DATA_PATH" \
+        2>&1 | xcbeautify --is-ci
+    if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+        echo "ERROR: Pod compilation failed in Stage 1. Check output above."
+        exit 1
+    fi
+else
+    xcodebuild build \
+        -workspace NativeTestHarness.xcworkspace \
+        -scheme "Pods-NativeTestHarness" \
+        -destination "platform=iOS Simulator,id=$UDID" \
+        -derivedDataPath "$DERIVED_DATA_PATH" \
+        -quiet
+    if [[ $? -ne 0 ]]; then
+        echo "ERROR: Pod compilation failed in Stage 1."
+        exit 1
+    fi
+fi
+
+echo ""
+echo "=== Running Tests (Stage 2) ==="
+# Run tests - pods cached from Stage 1, only incremental compilation needed
+if command -v xcbeautify &> /dev/null; then
+    xcodebuild test \
+        -workspace NativeTestHarness.xcworkspace \
+        -scheme "$SCHEME" \
+        -destination "platform=iOS Simulator,id=$UDID" \
+        -derivedDataPath "$DERIVED_DATA_PATH" \
+        2>&1 | xcbeautify --is-ci
+    if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+        echo "ERROR: Tests failed in Stage 2. Check output above."
+        exit 1
+    fi
+else
+    xcodebuild test \
+        -workspace NativeTestHarness.xcworkspace \
+        -scheme "$SCHEME" \
+        -destination "platform=iOS Simulator,id=$UDID" \
+        -derivedDataPath "$DERIVED_DATA_PATH"
+    if [[ $? -ne 0 ]]; then
+        echo "ERROR: Tests failed in Stage 2."
+        exit 1
+    fi
+fi
+
+echo ""
+echo "=== Tests Complete ==="

mobile-delight/react-native-fabric-rich-text

ios/FabricHTMLSanitizer.swift
@@ -1,0 +1,105 @@
+import Foundation
+import SwiftSoup
+
+/// HTML Sanitizer using SwiftSoup.
+///
+/// Provides XSS protection by sanitizing HTML content before
+/// it reaches the native iOS HTML parser (NSAttributedString).
+///
+/// Security approach:
+/// - Allowlist-based: Only allows known-safe tags and attributes
+/// - URL protocol validation: Only http, https, mailto, tel allowed
+/// - All event handlers and script content stripped
+@objc public final class FabricHTMLSanitizer: NSObject {
+  /**
+   * Allowed HTML tags - uses FabricGeneratedConstants (from src/core/constants.ts).
+   * Run `yarn codegen:constants` to regenerate.
+   */
+  private static var allowedTags: [String] { FabricGeneratedConstants.allowedTags }
+
+  /**
+   * Allowed URL protocols - uses FabricGeneratedConstants (from src/core/constants.ts).
+   */
+  private static var allowedProtocols: [String] { FabricGeneratedConstants.allowedProtocols }
+
+  /**
+   * Custom allowlist configured with our security policy.
+   * Note: SwiftSoup uses "Whitelist" class name; we use "allowlist" terminology.
+   */
+  private let allowlist: Whitelist
+
+  /**
+   * Output settings configured to disable pretty-printing.
+   * SwiftSoup's default pretty-print adds newlines/indentation between tags,
+   * which becomes unwanted text content when parsed.
+   */
+  private let outputSettings: OutputSettings
+
+  public override init() {
+    outputSettings = OutputSettings()
+    _ = outputSettings.prettyPrint(pretty: false)
+    // Start with no tags allowed, then add our safe list
+    allowlist = Whitelist.none()
+
+    // Add allowed tags
+    do {
+      for tag in FabricHTMLSanitizer.allowedTags {
+        try allowlist.addTags(tag)
+      }
+
+      // Allow href on anchor tags with protocol restrictions
+      try allowlist.addAttributes("a", "href")
+      for proto in FabricHTMLSanitizer.allowedProtocols {
+        try allowlist.addProtocols("a", "href", proto)
+      }
+
+      // Allow class attribute for styling (id removed per YAGNI - not used in rendering)
+      for tag in FabricHTMLSanitizer.allowedTags {
+        try allowlist.addAttributes(tag, "class")
+      }
+    } catch {
+      // Initialization errors should not occur with valid config
+      // Log error in debug builds
+      #if DEBUG
+        print("FabricHTMLSanitizer: Failed to configure allowlist: \(error)")
+      #endif
+    }
+
+    super.init()
+  }
+
+  /**
+   * Sanitize HTML content to remove XSS vectors.
+   *
+   * - Parameter html: The raw HTML string to sanitize
+   * - Returns: Sanitized HTML string safe for rendering, or empty string on error
+   */
+  @objc public func sanitize(_ html: String) -> String {
+    guard !html.isEmpty else {
+      return ""
+    }
+
+    do {
+      // Normalize inter-tag whitespace from source formatting (JSX indentation, etc.)
+      // BEFORE sanitizing so SwiftSoup processes clean input.
+      // Browsers ignore whitespace between block elements; we do the same.
+      let normalized = html.replacingOccurrences(
+        of: ">\\s+<",
+        with: "><",
+        options: .regularExpression
+      )
+
+      // Sanitize with pretty-printing disabled to prevent SwiftSoup from adding whitespace
+      var clean = try SwiftSoup.clean(normalized, "", allowlist, outputSettings) ?? ""
+
+      // Trim leading/trailing whitespace
+      clean = clean.trimmingCharacters(in: .whitespacesAndNewlines)
+
+      return clean
+    } catch {
+      // Log in all builds - silent failures cause debugging nightmares in production
+      NSLog("FabricHTMLSanitizer: Failed to sanitize HTML: %@", String(describing: error))
+      return ""
+    }
+  }
+}

ios/ci.sh
@@ -1,0 +1,175 @@
+#!/bin/bash
+set -euo pipefail
+
+SIMULATOR_NAME="iPhoneTest"
+SCHEME="FabricHtmlTextTests"
+
+# Navigate to the example app
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+EXAMPLE_IOS_DIR="$SCRIPT_DIR/../example/ios"
+
+cd "$EXAMPLE_IOS_DIR"
+
+# Get the latest available iOS runtime
+get_latest_ios_runtime() {
+    xcrun simctl list runtimes -j | \
+        python3 -c "
+import json, sys
+data = json.load(sys.stdin)
+ios_runtimes = [r for r in data['runtimes'] if r['platform'] == 'iOS' and r['isAvailable']]
+if ios_runtimes:
+    latest = sorted(ios_runtimes, key=lambda x: [int(p) for p in x['version'].split('.')], reverse=True)[0]
+    print(latest['identifier'])
+else:
+    sys.exit(1)
+"
+}
+
+# Get a compatible device type for the given runtime
+get_compatible_device_type() {
+    local runtime="$1"
+    xcrun simctl list devicetypes -j | \
+        python3 -c "
+import json, sys
+data = json.load(sys.stdin)
+runtime = '$runtime'
+
+# Extract major version from runtime (e.g., 'iOS-26-0' -> 26)
+import re
+match = re.search(r'iOS-(\d+)', runtime)
+runtime_major = int(match.group(1)) if match else 18
+
+# Modern device types that work with recent iOS versions
+# Prefer iPhone models from the last few years
+preferred_patterns = ['iPhone-16', 'iPhone-15', 'iPhone-14', 'iPhone-13', 'iPhone-12']
+
+for pattern in preferred_patterns:
+    for d in data['devicetypes']:
+        if pattern in d['identifier']:
+            print(d['identifier'])
+            sys.exit(0)
+
+# Fallback: any iPhone that's not too old
+for d in reversed(data['devicetypes']):
+    if 'iPhone' in d['name'] and 'SE' not in d['name']:
+        # Skip very old models
+        old_models = ['iPhone-4', 'iPhone-5', 'iPhone-6', 'iPhone-7', 'iPhone-8']
+        if not any(old in d['identifier'] for old in old_models):
+            print(d['identifier'])
+            sys.exit(0)
+
+# Last resort: any iPhone
+for d in reversed(data['devicetypes']):
+    if 'iPhone' in d['name']:
+        print(d['identifier'])
+        sys.exit(0)
+
+sys.exit(1)
+"
+}
+
+# Check if simulator exists and is available
+simulator_exists() {
+    xcrun simctl list devices -j | \
+        python3 -c "
+import json, sys
+data = json.load(sys.stdin)
+name = '$SIMULATOR_NAME'
+for runtime, devices in data['devices'].items():
+    for device in devices:
+        if device['name'] == name and device.get('isAvailable', False):
+            print(device['udid'])
+            sys.exit(0)
+sys.exit(1)
+" 2>/dev/null
+}
+
+echo "=== iOS CI Test Runner ==="
+echo "Working directory: $(pwd)"
+
+# Install CocoaPods dependencies
+echo ""
+echo "=== Installing CocoaPods Dependencies ==="
+bundle install --quiet
+bundle exec pod install
+
+# Check if simulator already exists
+if UDID=$(simulator_exists); then
+    echo "Using existing simulator: $SIMULATOR_NAME ($UDID)"
+else
+    echo "Creating simulator: $SIMULATOR_NAME"
+
+    RUNTIME=$(get_latest_ios_runtime)
+    echo "  Runtime: $RUNTIME"
+
+    DEVICE_TYPE=$(get_compatible_device_type "$RUNTIME")
+    echo "  Device type: $DEVICE_TYPE"
+
+    UDID=$(xcrun simctl create "$SIMULATOR_NAME" "$DEVICE_TYPE" "$RUNTIME")
+    echo "  Created: $UDID"
+fi
+
+# Custom DerivedData path for predictable caching
+# Configurable via environment variable for CI flexibility
+DERIVED_DATA_PATH="${XCODE_DERIVED_DATA_PATH:-$EXAMPLE_IOS_DIR/DerivedData}"
+mkdir -p "$DERIVED_DATA_PATH" || {
+    echo "ERROR: Cannot create DerivedData directory at $DERIVED_DATA_PATH"
+    exit 1
+}
+
+echo ""
+echo "=== Building Dependencies (Stage 1) ==="
+# Build pods scheme first - this compiles all React Native dependencies
+# and is cached by irgaly/xcode-cache action in CI
+if command -v xcbeautify &> /dev/null; then
+    xcodebuild build \
+        -workspace FabricHtmlTextExample.xcworkspace \
+        -scheme "Pods-FabricHtmlTextExample" \
+        -destination "platform=iOS Simulator,id=$UDID" \
+        -derivedDataPath "$DERIVED_DATA_PATH" \
+        2>&1 | xcbeautify --is-ci
+    if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+        echo "ERROR: Pod compilation failed in Stage 1. Check output above."
+        exit 1
+    fi
+else
+    xcodebuild build \
+        -workspace FabricHtmlTextExample.xcworkspace \
+        -scheme "Pods-FabricHtmlTextExample" \
+        -destination "platform=iOS Simulator,id=$UDID" \
+        -derivedDataPath "$DERIVED_DATA_PATH" \
+        -quiet
+    if [[ $? -ne 0 ]]; then
+        echo "ERROR: Pod compilation failed in Stage 1."
+        exit 1
+    fi
+fi
+
+echo ""
+echo "=== Running Tests (Stage 2) ==="
+# Run tests - pods cached from Stage 1, only incremental compilation needed
+if command -v xcbeautify &> /dev/null; then
+    xcodebuild test \
+        -workspace FabricHtmlTextExample.xcworkspace \
+        -scheme "$SCHEME" \
+        -destination "platform=iOS Simulator,id=$UDID" \
+        -derivedDataPath "$DERIVED_DATA_PATH" \
+        2>&1 | xcbeautify --is-ci
+    if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+        echo "ERROR: Tests failed in Stage 2. Check output above."
+        exit 1
+    fi
+else
+    xcodebuild test \
+        -workspace FabricHtmlTextExample.xcworkspace \
+        -scheme "$SCHEME" \
+        -destination "platform=iOS Simulator,id=$UDID" \
+        -derivedDataPath "$DERIVED_DATA_PATH"
+    if [[ $? -ne 0 ]]; then
+        echo "ERROR: Tests failed in Stage 2."
+        exit 1
+    fi
+fi
+
+echo ""
+echo "=== Tests Complete ==="